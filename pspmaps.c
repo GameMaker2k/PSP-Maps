23b8fa86e367334f475fd2e5a93e4a1e705c92f7
Antoine Jacquet <royale@zerezo.com>
2008-03-11 00:00:00 +0100

    version 0.2
    * added a menu to select wireless connection
    * the map loads faster
    * you can now use the analog stick for smooth moves
    * added a cache to go back quickly to the previous views

--- pspmaps.c
+++ pspmaps.c
@@ -5,6 +5,7 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <SDL.h>
 #include <SDL_image.h>
@@ -13,20 +14,39 @@
 #define WIDTH 480
 #define HEIGHT 272
 #define BPP 32
+#define BUFFER_SIZE 100 * 1024
+#define CACHE_SIZE 32
+#define DIGITAL_STEP 0.1
+#define JOYSTICK_STEP 0.05
 
 #ifdef _PSP_FW_VERSION
 #include <pspkernel.h>
+#include <pspsdk.h>
+#include <psputility.h>
+#include <pspnet_apctl.h>
 #define printf pspDebugScreenPrintf
 #define MODULE_NAME "PSP-Maps"
-PSP_MODULE_INFO(MODULE_NAME, 0, 1, 0);
-PSP_HEAP_SIZE_KB(12*1024);
-PSP_MAIN_THREAD_ATTR(0);
-PSP_MAIN_THREAD_STACK_SIZE_KB(32);
+PSP_MODULE_INFO(MODULE_NAME, 0, 1, 1);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
+PSP_HEAP_SIZE_KB(20480);
+#include "netdialog.c"
+#else
+static int running = 1;
 #endif
 
 SDL_Surface *screen;
-int x = 1, y = 1, z = 16, s = 0;
+SDL_Surface *loading;
+int z = 16, s = 0;
+float x = 1, y = 1;
 CURL* curl;
+
+struct _cache
+{
+	int loaded;
+	int x, y, z, s;
+	SDL_Surface *tile;
+} cache[CACHE_SIZE];
+int cur = 0;
 
 enum
 {
@@ -88,33 +108,69 @@ void sattile(int x, int y, int z, char *b)
 	b[0] = 't';
 }
 
-/* downloads the image from Google for location (x,y,z) with mode (s) to file "temp" */
-void gettile(int x, int y, int z, int s)
+size_t curl_write(void *ptr, size_t size, size_t nb, SDL_RWops *rw)
 {
-	char buffer[1024];
-	FILE *fic;
-	fic = fopen("temp", "wb");
+	int t = nb * size;
+	rw->write(rw, ptr, size, nb);
+	return t;
+}
+
+/* downloads the image from Google for location (x,y,z) with mode (s) to file "temp" */
+SDL_Surface* gettile(int x, int y, int z, int s)
+{
+	char request[1024], response[BUFFER_SIZE];
+	SDL_RWops *rw;
+	SDL_Surface *tmp;
+	
+	int i;
+	for (i = 0; i < CACHE_SIZE; i++)
+		if (cache[i].loaded && cache[i].x == x && cache[i].y == y && cache[i].z == z && cache[i].s == s)
+			return cache[i].tile;
+	
+	SDL_Rect r;
+	r.x = 160;
+	r.y = 110;
+	SDL_BlitSurface(loading, NULL, screen, &r);
+	SDL_Flip(screen);
+	
 	switch (s)
 	{
 		case GG_MAP:
-			sprintf(buffer, "http://mt0.google.com/mt?n=404&v=w2.69&x=%d&y=%d&zoom=%d", x, y, z);
+			sprintf(request, "http://mt0.google.com/mt?n=404&v=w2.69&x=%d&y=%d&zoom=%d", x, y, z);
 			break;
 		case GG_TERRAIN:
-			sprintf(buffer, "http://mt0.google.com/mt?n=404&v=w2p.64&x=%d&y=%d&zoom=%d", x, y, z);
+			sprintf(request, "http://mt0.google.com/mt?n=404&v=w2p.64&x=%d&y=%d&zoom=%d", x, y, z);
 			break;
 		case GG_SATELLITE:
-			sprintf(buffer, "http://kh0.google.com/kh?n=404&v=20&t=");
-			sattile(x, y, z, buffer + strlen(buffer));
+			sprintf(request, "http://kh0.google.com/kh?n=404&v=20&t=");
+			sattile(x, y, z, request + strlen(request));
 			break;
 	}
 	
+	rw = SDL_RWFromMem(response, BUFFER_SIZE);
+	
 	//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
-	curl_easy_setopt(curl, CURLOPT_URL, buffer);
-	curl_easy_setopt(curl, CURLOPT_FILE, fic);
+	curl_easy_setopt(curl, CURLOPT_URL, request);
+	//curl_easy_setopt(curl, CURLOPT_FILE, fic);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, rw);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5);
 	curl_easy_perform(curl);
 	
-	fclose(fic);
+	rw->seek(rw, 0, SEEK_SET);
+	tmp = IMG_Load_RW(rw, 1);
+	
+	if (cache[cur].loaded) SDL_FreeSurface(cache[cur].tile);
+	cache[cur].loaded = 1;
+	cache[cur].x = x;
+	cache[cur].y = y;
+	cache[cur].z = z;
+	cache[cur].s = s;
+	cache[cur].tile = tmp;
+	cur = (cur + 1) % CACHE_SIZE;
+	
+	return tmp;
 }
 
 /* updates the display */
@@ -124,247 +180,191 @@ void display()
 	SDL_Rect r;
 	int i, j;
 	
-	#ifndef _PSP_FW_VERSION
-	printf("Display: x=%d y=%d z=%d s=%d\n", x, y, z, s);
-	#endif
+	if (x < 1) x = 1;
+	if (x > pow(2, 17-z)-1) x = pow(2, 17-z)-1;
+	if (y < 1) y = 1;
+	if (y > pow(2, 17-z)-1) y = pow(2, 17-z)-1;
 	
-	for (j = -1; j < 1; j++)
-		for (i = -1; i < 1; i++)
+	// pre-load the tiles
+	for (j = y-1; j < y+1; j++)
+		for (i = x-1; i < x+1; i++)
 		{
-			r.x = WIDTH/2 + i*256;
-			r.y = HEIGHT/2 + j*256;
-			gettile(x+i, y+j, z, s);
-			tmp = IMG_Load("temp");
+			//~ r.x = WIDTH/2 + (i-x)*256;
+			//~ r.y = HEIGHT/2 + (j-y)*256;
+			tmp = gettile(i, j, z, s);
+			//~ SDL_BlitSurface(tmp, NULL, screen, &r);
+		}
+	
+	// display the tiles
+	for (j = y-1; j < y+1; j++)
+		for (i = x-1; i < x+1; i++)
+		{
+			r.x = WIDTH/2 + (i-x)*256;
+			r.y = HEIGHT/2 + (j-y)*256;
+			tmp = gettile(i, j, z, s);
 			SDL_BlitSurface(tmp, NULL, screen, &r);
-			SDL_FreeSurface(tmp);
 		}
 	
 	SDL_Flip(screen);
 }
 
 /* main loop */
-void go()
+void mainLoop()
 {
-	int flags, finish = 0;
+	int flags;
 	SDL_Event event;
+	SDL_Joystick *joystick;
+	
+	bzero(cache, sizeof(cache));
 	
 	curl = curl_easy_init();
 	
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) == -1)
 		quit();
 	
-	SDL_JoystickOpen(0);
+	joystick = SDL_JoystickOpen(0);
 	SDL_JoystickEventState(SDL_ENABLE);
 	
 	flags = SDL_HWSURFACE | SDL_ANYFORMAT | SDL_DOUBLEBUF;
-	//flags |= SDL_FULLSCREEN;
 	screen = SDL_SetVideoMode(WIDTH, HEIGHT, BPP, flags);
-		
+	
 	if (screen == NULL)
 		quit();
 	
-	display();
+	loading = IMG_Load("loading.png");
 	
-	while (!finish)
-	while (SDL_PollEvent(&event))
+	//display();
+	
+	while (running)
 	{
-		switch (event.type)
+		while (SDL_PollEvent(&event))
 		{
-			case SDL_QUIT:
-				finish = 1;
-				break;
-			case SDL_KEYDOWN:
-			case SDL_JOYBUTTONDOWN:
-				if (event.type == SDL_KEYDOWN)
-					flags = event.key.keysym.sym;
-				else
-					flags = event.jbutton.button;
-				switch (flags)
-				{
-					case SDLK_LEFT:
-					case PSP_BUTTON_LEFT:
-						if (x > 1)
-						{
-							x--;
-							display();
-						}
-						break;
-					case SDLK_RIGHT:
-					case PSP_BUTTON_RIGHT:
-						if (x < pow(2, 17-z)-1)
-						{
-							x++;
-							display();
-						}
-						break;
-					case SDLK_UP:
-					case PSP_BUTTON_UP:
-						if (y > 1)
-						{
-							y--;
-							display();
-						}
-						break;
-					case SDLK_DOWN:
-					case PSP_BUTTON_DOWN:
-						if (y < pow(2, 17-z)-1)
-						{
-							y++;
-							display();
-						}
-						break;
-					case SDLK_PAGEUP:
-					case PSP_BUTTON_R:
-						if (z > 1 || (s && z > -4))
-						{
-							z--;
-							x*=2;
-							y*=2;
-							display();
-						}
-						break;
-					case SDLK_PAGEDOWN:
-					case PSP_BUTTON_L:
-						if (z < 16)
-						{
-							z++;
-							x/=2;
-							y/=2;
-							display();
-						}
-						break;
-					case SDLK_SPACE:
-					case PSP_BUTTON_A:
-					case PSP_BUTTON_B:
-					case PSP_BUTTON_X:
-					case PSP_BUTTON_Y:
-						s = (s + 1) % GG_NUM_VIEWS;
-						/* zoom out if needed */
-						while (z < 1)
-						{
-							x/=2;
-							y/=2;
-							z++;
-						}
-						display();
-						break;
-					case SDLK_ESCAPE:
-					case PSP_BUTTON_START:
-						#ifdef _PSP_FW_VERSION
-						sceKernelExitGame();
-						#endif
-						quit();
-						break;
-					default:
-						break;
-				}
-				break;
+			switch (event.type)
+			{
+				case SDL_QUIT:
+					running = 0;
+					break;
+				case SDL_KEYDOWN:
+				case SDL_JOYBUTTONDOWN:
+					if (event.type == SDL_KEYDOWN)
+						flags = event.key.keysym.sym;
+					else
+						flags = event.jbutton.button;
+					switch (flags)
+					{
+						case SDLK_LEFT:
+						case PSP_BUTTON_LEFT:
+							x -= DIGITAL_STEP;
+							//display();
+							break;
+						case SDLK_RIGHT:
+						case PSP_BUTTON_RIGHT:
+							x += DIGITAL_STEP;
+							//display();
+							break;
+						case SDLK_UP:
+						case PSP_BUTTON_UP:
+							y -= DIGITAL_STEP;
+							//display();
+							break;
+						case SDLK_DOWN:
+						case PSP_BUTTON_DOWN:
+							y += DIGITAL_STEP;
+							//display();
+							break;
+						case SDLK_PAGEUP:
+						case PSP_BUTTON_R:
+							if (z > 1 || (s && z > -4))
+							{
+								z--;
+								x*=2;
+								y*=2;
+								//display();
+							}
+							break;
+						case SDLK_PAGEDOWN:
+						case PSP_BUTTON_L:
+							if (z < 16)
+							{
+								z++;
+								x/=2;
+								y/=2;
+								//display();
+							}
+							break;
+						case SDLK_SPACE:
+						case PSP_BUTTON_A:
+						case PSP_BUTTON_B:
+						case PSP_BUTTON_X:
+						case PSP_BUTTON_Y:
+							s = (s + 1) % GG_NUM_VIEWS;
+							/* zoom out if needed */
+							while (z < 1)
+							{
+								x/=2;
+								y/=2;
+								z++;
+							}
+							//display();
+							break;
+						case SDLK_ESCAPE:
+						case PSP_BUTTON_START:
+							running = 0;
+							break;
+						default:
+							break;
+					}
+					break;
+			}
 		}
-		SDL_Delay(100);
+		
+		x += SDL_JoystickGetAxis(joystick, 0) / (32768/16) / 16.0 * JOYSTICK_STEP;
+		y += SDL_JoystickGetAxis(joystick, 1) / (32768/16) / 16.0 * JOYSTICK_STEP;
+		
+		display();
+		SDL_Delay(50);
 	}
 }
-
-#ifdef _PSP_FW_VERSION
-/* Connect to an access point */
-int connect_to_apctl(int config)
-{
-	int err;
-	int stateLast = -1;
-
-	/* Connect using the first profile */
-	err = sceNetApctlConnect(config);
-	if (err != 0)
-	{
-		printf(MODULE_NAME ": sceNetApctlConnect returns %08X\n", err);
-		return 0;
-	}
-
-	printf(MODULE_NAME ": Connecting...\n");
-	while (1)
-	{
-		int state;
-		err = sceNetApctlGetState(&state);
-		if (err != 0)
-		{
-			printf(MODULE_NAME ": sceNetApctlGetState returns $%x\n", err);
-			break;
-		}
-		if (state > stateLast)
-		{
-			printf("  connection state %d of 4\n", state);
-			stateLast = state;
-		}
-		if (state == 4)
-			break;  // connected with static IP
-
-		// wait a little before polling again
-		sceKernelDelayThread(50*1000); // 50ms
-	}
-	printf(MODULE_NAME ": Connected!\n");
-
-	if(err != 0)
-	{
-		return 0;
-	}
-
-	return 1;
-}
-
-int net_thread(SceSize args, void *argp)
-{
-	int err;
-
-	do
-	{
-		if((err = pspSdkInetInit()))
-		{
-			printf(MODULE_NAME ": Error, could not initialise the network %08X\n", err);
-			break;
-		}
-
-		if(connect_to_apctl(1))
-		{
-			// connected, get my IPADDR and run test
-			char szMyIPAddr[32];
-			if (sceNetApctlGetInfo(8, szMyIPAddr) != 0)
-				strcpy(szMyIPAddr, "unknown IP address");
-
-			go();
-		}
-	}
-	while(0);
-
-	return 0;
-}
-#endif
 
 int main(void)
 {
 	#ifdef _PSP_FW_VERSION
-	SceUID thid;
 	pspDebugScreenInit();
 	#endif
 	printf(
-		"PSP-Maps 0.1\n"
-		"http://royale.zerezo.com/psp/\n"
 		"\n"
-		"This software is using Google Maps images.\n"
 		"\n"
+		"\n"
+		"        _____   _____ _____        __  __                     \n"
+		"       |  __ \\ / ____|  __ \\      |  \\/  |                    \n"
+		"       | |__) | (___ | |__) |_____| \\  / | __ _ _ __  ___     \n"
+		"       |  ___/ \\___ \\|  ___/______| |\\/| |/ _` | '_ \\/ __|    \n"
+		"       | |     ____) | |          | |  | | (_| | |_) \\__ \\    \n"
+		"       |_|    |_____/|_|          |_|  |_|\\__,_| .__/|___/    \n"
+		"                                               | |            \n"
+		"                                               |_|            \n"
+		"\n"
+		"\n"
+		"           PSP-Maps 0.2\n"
+		"\n"
+		"           http://royale.zerezo.com/psp/\n"
+		"\n"
+		"\n"
+		"           This software is using Google Maps images.\n"
 	);
+	SDL_Delay(3000);
 	#ifdef _PSP_FW_VERSION
-	sceUtilityLoadNetModule(1);
-	sceUtilityLoadNetModule(3);
-	thid = sceKernelCreateThread("net_thread", net_thread, 0x18, 0x10000, PSP_THREAD_ATTR_USER, NULL);
-	if(thid < 0)
-	{
-		printf("Error, could not create thread\n");
-		sceKernelSleepThread();
-	}
-	sceKernelStartThread(thid, 0, NULL);
-	sceKernelExitDeleteThread(0);
+	sceUtilityLoadNetModule(PSP_NET_MODULE_COMMON);
+	sceUtilityLoadNetModule(PSP_NET_MODULE_INET);
+	netInit();
+	SetupCallbacks();
+	setupGu();
+	netDialog();
+	mainLoop();
+	sceKernelExitGame();
+	netTerm();
 	#else
-	go();
+	mainLoop();
 	#endif
-	
 	return 0;
 }
